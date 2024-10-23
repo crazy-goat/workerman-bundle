561bf055f7c5973e66626c3e05d4990e54691b9e
Piotr Hałas <piohal@boozt.com>
2024-10-23 11:19:06 +0200

    Add serve_files config option to control file serving behavior
    Introduced a `serve_files` configuration option in the server worker to control whether files should be served from the public directory. This change updates the `ServerWorker` and `HttpRequestHandler` to respect the `serve_files` setting, enhancing flexibility for file serving.

--- src/Worker/ServerWorker.php
+++ src/Worker/ServerWorker.php
@@ -6,6 +6,8 @@ namespace Luzrain\WorkermanBundle\Worker;
 
 use Luzrain\WorkermanBundle\KernelFactory;
 use Luzrain\WorkermanBundle\Utils;
+use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http\Request;
 use Workerman\Worker;
 
 final class ServerWorker
@@ -47,10 +49,18 @@ final class ServerWorker
         $worker->count = $serverConfig['processes'] ?? Utils::cpuCount() * 2;
         $worker->transport = $transport;
         $worker->onWorkerStart = function (Worker $worker) use ($kernelFactory, $serverConfig) {
+            $serveFiles = $serverConfig['serve_files'] ?? true;
             $worker->log(sprintf('%s "%s" started', $worker->name, $serverConfig['name']));
             $kernel = $kernelFactory->createKernel();
             $kernel->boot();
-            $worker->onMessage = $kernel->getContainer()->get('workerman.http_request_handler');
+            $worker->onMessage =
+                function (TcpConnection $connection, Request $workermanRequest) use ($serveFiles, $kernel): void {
+                    $kernel->getContainer()->get('workerman.http_request_handler')(
+                        $connection,
+                        $workermanRequest,
+                        $serveFiles
+                    );
+                };
         };
     }
 }
