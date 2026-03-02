66fbb3d77d3123e5561780378a1f46718e9035f9
Mickeywaugh <mickeywaugh@163.com>
2026-03-02 16:33:40 +0800

    feat(workerman): 添加Workerman命令支持 - 新增src/Command/WorkermanCommand.php文件，实现Workerman命令行工具 - 支持start|stop|restart|reload|status|connections操作 - 支持-daemon模式运行选项

---
+++ src/Command/WorkermanCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'workerman', description: 'cmd for workerman bundle')]
+class WorkermanCommand extends Command
+{
+    protected $pidFile = 'var/run/workerman.pid';
+    protected $channelName = 'Workerman';
+    private string $runtime;
+    private string $phpCmd;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->runtime = 'APP_RUNTIME=CrazyGoat\\\\WorkermanBundle\\\\Runtime';
+        $this->phpCmd = 'php ./public/index.php';
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('action', InputArgument::REQUIRED, 'Action: start|stop|restart|reload|status|connections')
+            ->addOption('daemon', 'd', InputOption::VALUE_NONE, '-d Run in daemon mode')
+        ;
+    }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $action = $input->getArgument('action');
+        if ($input->getOption('daemon')) {
+            $cmd = sprintf('%s %s %s -d > /dev/null 2>&1 &', $this->runtime, $this->phpCmd, $action);
+        } else {
+            $cmd = sprintf('%s %s %s', $this->runtime, $this->phpCmd, $action);
+        }
+        passthru($cmd, $returnCode);
+        if ($returnCode !== 0) {
+            $io->error(sprintf("Failed to execute cmd %s with exit code %s \n", $cmd, $returnCode,));
+        }
+        $io->success(sprintf("Workerman Http Server started. \n"));
+        return Command::SUCCESS;
+    }
+}
