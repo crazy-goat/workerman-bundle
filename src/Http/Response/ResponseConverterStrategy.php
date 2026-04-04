2812cb4d62f2a887e838a4dd905eca26c932bf83
Piotr Hałas <piotr.h@decodo.com>
2026-04-04 15:57:41 +0200

    feat: add ResponseConverterStrategy interface

---
+++ src/Http/Response/ResponseConverterStrategy.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CrazyGoat\WorkermanBundle\Http\Response;
+
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Workerman\Protocols\Http\Response as WorkermanResponse;
+
+interface ResponseConverterStrategy
+{
+    /**
+     * Check if this strategy can handle the given response.
+     */
+    public function supports(SymfonyResponse $response): bool;
+
+    /**
+     * Convert Symfony response to Workerman response.
+     *
+     * @param array<string, list<string|null>> $headers Pre-extracted headers
+     */
+    public function convert(SymfonyResponse $response, array $headers): WorkermanResponse;
+}
