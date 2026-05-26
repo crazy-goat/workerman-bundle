5ed90c943f9f65ad9d8dcd97fb89c6f32cc7b2b1
Piotr Hałas <halaspiotr@gmail.com>
2026-05-26 21:16:41 +0200

    refactor: split SfxDownloader::extractZip into staged methods with typed exception (#251) (#430)
    Co-authored-by: Piotr Hałas <piotr.h@decodo.com>

---
+++ src/Exception/SfxExtractionException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CrazyGoat\WorkermanBundle\Exception;
+
+final class SfxExtractionException extends \RuntimeException implements WorkermanExceptionInterface
+{
+}
